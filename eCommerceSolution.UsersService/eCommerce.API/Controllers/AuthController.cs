1f17d96195acdb9a77a5dcd41f98f62ff7bf8723
kamranrafiqkhan <kamranrafiqkhan@gmail.com>
2025-01-26 20:23:43 +0500

    Initial commit

---
+++ eCommerceSolution.UsersService/eCommerce.API/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using eCommerce.Core.DTO;
+using eCommerce.Core.ServiceContracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")] //api/auth
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUsersService _userService;
+        public AuthController(IUsersService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")] //POST api/auth/register
+        public async Task<IActionResult> Register(RegisterRequest registerRequest)
+        {
+            if (registerRequest == null)
+            {
+                return BadRequest("Invalid registeration data");
+            }
+
+            AuthenticationResponse? response = await _userService.Register(registerRequest);
+
+            if (response == null || response.Success == false)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
+        {
+            if (loginRequest == null)
+            {
+                return BadRequest("Invalid login data");
+            }
+
+            AuthenticationResponse? response = await _userService.Login(loginRequest);
+
+            if(response == null || response.Success == false)
+            {
+                return Unauthorized(response);
+            }
+
+            return Ok(response);
+            
+        }
+    }
+}
