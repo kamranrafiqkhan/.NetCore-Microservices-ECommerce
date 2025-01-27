7675ae29bb87d3c9019f60735830458ab38f752c
kamranrafiqkhan <kamranrafiqkhan@gmail.com>
2025-01-27 19:46:45 +0500

    Commit #2

--- eCommerceSolution.UsersService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
+++ eCommerceSolution.UsersService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
@@ -1,6 +1,8 @@
-﻿using eCommerce.Core.DTO;
+﻿using Dapper;
+using eCommerce.Core.DTO;
 using eCommerce.Core.Entities;
 using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.DbContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +13,30 @@ namespace eCommerce.Infrastructure.Repositories;
 
 internal class UsersRepository : IUsersRepository
 {
+    private readonly DapperDbContext _dbContext;
+
+    public UsersRepository(DapperDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public async Task<ApplicationUser?> AddUser(ApplicationUser user)
     {
         //Generate a new unique user ID for the user
         user.UserID = Guid.NewGuid();
-        return user;
+
+        //SQL Query to isert user data into the "Users" table
+        string query = "INSERT INTO public. \"Users\"(\"UserID\", \"Email\", \"PersonName\", \"Gender\", \"Password\") VALUES(@UserID, @Email, @PersonName, @Gender, @Password)";
+
+        int rowCountAffected = await _dbContext.DbConnection.ExecuteAsync(query, user);
+
+        if (rowCountAffected > 0)
+        {
+            return user;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     public async Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password)
