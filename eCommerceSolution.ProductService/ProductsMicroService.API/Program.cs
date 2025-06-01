8c2c185cf826949b886580636376450e0e7e06d4
kamranrafiqkhan <kamranrafiqkhan@gmail.com>
2025-06-01 21:34:06 +0500

    Commit #6

--- eCommerceSolution.ProductService/ProductsMicroService.API/Program.cs
+++ eCommerceSolution.ProductService/ProductsMicroService.API/Program.cs
@@ -22,12 +22,39 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 
+//Add Swagger service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Products API", Version = "v1" });
+});
+
+
+//Cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
 
+//Cors
+app.UseCors();
+
+//Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
 //Authentication
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
